480818239c87b34d6bd93ccd53b2ed76670d4279
Mario Poveda <mnpoveda@unicesar.edu.co>
2026-05-17 18:24:17 -0500

    chore: scaffold inicial

---
+++ src/main/java/com/ajedrez/app/AjedrezApplication.java
@@ -0,0 +1,11 @@
+package com.ajedrez.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjedrezApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AjedrezApplication.class, args);
+    }
+}
